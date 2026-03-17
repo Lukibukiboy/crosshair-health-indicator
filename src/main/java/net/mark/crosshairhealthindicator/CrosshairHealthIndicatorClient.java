07599b60346bd1f8d0e464db883349c40756fc43
Mark <3mark.7395g@gmail.com>
2026-03-17 20:42:27 +0100

    - added mod description and cloth dependency in fabric.mod.json - added icon - replaced toml config with json

--- src/main/java/net/mark/crosshairhealthindicator/CrosshairHealthIndicatorClient.java
+++ src/main/java/net/mark/crosshairhealthindicator/CrosshairHealthIndicatorClient.java
@@ -1,7 +1,7 @@
 package net.mark.crosshairhealthindicator;
 
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
@@ -9,7 +9,7 @@ import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.GameType;
 
 public class CrosshairHealthIndicatorClient implements ClientModInitializer {
@@ -18,12 +18,12 @@ public class CrosshairHealthIndicatorClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        AutoConfig.register(ModConfig.class, Toml4jConfigSerializer::new);
+        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
         config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 
         HudElementRegistry.attachElementAfter(
                 VanillaHudElements.CROSSHAIR,
-                Identifier.fromNamespaceAndPath(MOD_ID, "crosshair"),
+                ResourceLocation.fromNamespaceAndPath(MOD_ID, "crosshair"),
                 CrosshairHealthIndicatorClient::renderIndicator
         );
     }
