dd83e79ecd8180134274f2a6c753a0fee37d83b4
Mark <3mark.7395g@gmail.com>
2026-03-18 02:05:23 +0100

    added an option to disable precise health values

--- src/main/java/net/mark/crosshairhealthindicator/ModConfig.java
+++ src/main/java/net/mark/crosshairhealthindicator/ModConfig.java
@@ -12,9 +12,10 @@ public class ModConfig implements ConfigData {
 
     public boolean alwaysShow = false;
 
-
     @ConfigEntry.Gui.Excluded
     public static final int TEXT_COLOR_DEFAULT = 0xFFFFFF;
     @ConfigEntry.ColorPicker
     public int textColor = TEXT_COLOR_DEFAULT;
+
+    public boolean preciseHealth = true;
 }
