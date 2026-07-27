ecdfada9f7077a6ae69802005a75991a1faeea7a
Mark <3mark.7395g@gmail.com>
2026-07-28 00:48:11 +0200

    added colapsable section for health warning color configuration

--- src/main/java/net/mark/crosshairhealthindicator/ModConfig.java
+++ src/main/java/net/mark/crosshairhealthindicator/ModConfig.java
@@ -12,11 +12,24 @@ public class ModConfig implements ConfigData {
 
     public boolean alwaysShow = false;
 
-
     @ConfigEntry.Gui.Excluded
     public static final int TEXT_COLOR_DEFAULT = 0xFFFFFF;
     @ConfigEntry.ColorPicker
     public int textColor = TEXT_COLOR_DEFAULT;
 
     public boolean preciseHealth = true;
+
+    @ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
+    WarningColor warningColor = new WarningColor();
+    public static class WarningColor implements ConfigData {
+
+        public boolean enableWarningColor = false;
+
+        @ConfigEntry.Gui.Excluded
+        public static final int WARNING_COLOR_DEFAULT = 0xFF0000;
+        @ConfigEntry.ColorPicker
+        public int warningColor = WARNING_COLOR_DEFAULT;
+
+        public int changeColorBelowHealth = 6;
+    }
 }
